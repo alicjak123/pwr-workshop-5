055c2985830cd947afae984052e60e23dc466c66
Alicja <alicja_k123@o2.pl>
2014-12-13 21:41:51 +0100

    first ex

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    return @current_user if defined?(@current_user)
-    @current_user = User.find_or_create_by!(
-      email: 'user@example.com'
-    )
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
